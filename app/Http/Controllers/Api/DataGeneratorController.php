94e4d5096b8a55b2953faeca2f4dbf1bcf135f73
PANGSORAMDEPO <pangsoramdepo@gmail.com>
2021-04-09 13:39:37 +0700

    Generate Data Ejabberd

---
+++ app/Http/Controllers/Api/DataGeneratorController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Euser;
+
+class DataGeneratorController extends Controller
+{
+    public function data()
+    {
+        $startFrom = request('startFrom');
+        $inserts = [];
+        for($i=$startFrom; $i< $startFrom + 1000; $i++) {
+            array_push($inserts,array(
+                'username'  => 85519000000 + $i,
+                'password'  => 'tsungloadtest',
+                'iterationcount'    => 0
+            ));
+        }
+        return Euser::insert($inserts);
+    }
+
+    public function count()
+    {
+        return Euser::count();
+    }
+
+    public function last()
+    {
+        return Euser::where('password','tsungloadtest')->orderBy('username','DESC')->take(5)->get();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\DataGeneratorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/generate/{startFrom}', [DataGeneratorController::class, 'data']);
+Route::get('/count', [DataGeneratorController::class, 'count']);
+Route::get('/last', [DataGeneratorController::class, 'last']);
